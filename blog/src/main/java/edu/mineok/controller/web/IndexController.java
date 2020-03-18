f78c5414ac007b835b82baf5d0228fe161c1e8a8
高明 <mineok@qq.com>
2020-03-18 18:50:32 +0800

    初步完成,细节日后继续优化

--- blog/src/main/java/edu/mineok/controller/web/IndexController.java
+++ blog/src/main/java/edu/mineok/controller/web/IndexController.java
@@ -53,4 +53,10 @@ public class IndexController {
         model.addAttribute("query", query);
         return "search";
     }
+
+    @GetMapping("/footer/newblog")
+    public String newBlogs(Model model){
+        model.addAttribute("newblogs",blogService.listRecommendBlogTop(3));
+        return "_fragments :: newblogList";
+    }
 }
