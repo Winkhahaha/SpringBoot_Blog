e75ea34607c3376c7fee5d2b8874442f0b3b3228
Ming Gao <thoughtworks.gm.gao@daimler.com>
2021-05-25 15:55:51 +0800

    Revert "[Ming Gao] feat: Add File Test."

--- blog/src/test/java/edu/mineok/BlogApplicationTests.java
+++ blog/src/test/java/edu/mineok/BlogApplicationTests.java
@@ -2,22 +2,13 @@ package edu.mineok;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.FileCopyUtils;
-
-import java.io.File;
-import java.io.IOException;
 
 @SpringBootTest
 class BlogApplicationTests {
 
     @Test
-    void contextLoads() throws IOException {
-
-            File file = new File("/Users/minggao/Ming Gao/SpringBoot_Blog/blog/1.txt");
-            File file1 = new File("/Users/minggao/Ming Gao/SpringBoot_Blog/blog/2.txt");
-            FileCopyUtils.copy(file, file1);
-            System.out.println("finish");
-        }
-
+    void contextLoads() {
+        System.out.println();
+    }
 
 }
