61cd9ac74dc1997284d84da814cd8523da58cb85
Ming Gao <thoughtworks.gm.gao@daimler.com>
2021-05-25 15:53:50 +0800

    [Ming Gao] feat: Add Test.

--- blog/src/test/java/edu/mineok/BlogApplicationTests.java
+++ blog/src/test/java/edu/mineok/BlogApplicationTests.java
@@ -2,12 +2,22 @@ package edu.mineok;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.FileCopyUtils;
+
+import java.io.File;
+import java.io.IOException;
 
 @SpringBootTest
 class BlogApplicationTests {
 
     @Test
-    void contextLoads() {
-    }
+    void contextLoads() throws IOException {
+
+            File file = new File("/Users/minggao/Ming Gao/SpringBoot_Blog/blog/1.txt");
+            File file1 = new File("/Users/minggao/Ming Gao/SpringBoot_Blog/blog/2.txt");
+            FileCopyUtils.copy(file, file1);
+            System.out.println("finish");
+        }
+
 
 }
